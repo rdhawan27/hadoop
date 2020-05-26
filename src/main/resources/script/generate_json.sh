8bd1137b85b8f5794baad8a466710d12ebc13b47
dinesh sachdev <dineshsachde@gmail.com>
2020-05-26 18:02:03 -0500

    updated code

--- src/main/resources/script/generate_json.sh
+++ src/main/resources/script/generate_json.sh
@@ -1,2 +1,10 @@
-CLASSPATH=./dsacd.jar
-java -cp $CLASSPATH biz.ds.www.utils.json.WriterJSON $1 $2 $3
+#!/bin/sh
+####
+#Description- This shell script invokes simulator that generates random Json data
+#It takes 3 parameters-
+# 1 - log4j.properties
+# 2 - Number of Threads
+# 3 - Sleep time for each Thread
+####
+CLASSPATH=./lib/dsacd.jar
+java -cp $CLASSPATH biz.ds.www.utils.json.WriterJSON ./log4j.properties 8 5
